b022ab4e777fa693620a318a8fab274447642134
ADITYA VERMA <adityav1304@gmail.com>
2024-09-20 15:57:49 +0530

    Time: 8 ms (43.12%), Space: 9.6 MB (92.97%) - LeetHub

--- 0214-shortest-palindrome/0214-shortest-palindrome.cpp
+++ 0214-shortest-palindrome/0214-shortest-palindrome.cpp
@@ -1,37 +1,13 @@
 class Solution {
 public:
     string shortestPalindrome(string s) {
-        int n = s.size();
-        
-        // Reverse the input string
-        string rev_s = s;
-        reverse(rev_s.begin(), rev_s.end());
-        
-        // Combine original string and reversed string with a separator
-        string combined = s + "#" + rev_s;
-        
-        // KMP partial match table for the combined string
-        vector<int> kmp_table(combined.size(), 0);
-        
-        // Build the KMP table (longest prefix that is also a suffix)
-        for (int i = 1; i < combined.size(); ++i) {
-            int j = kmp_table[i - 1];
-            
-            while (j > 0 && combined[i] != combined[j]) {
-                j = kmp_table[j - 1];
-            }
-            
-            if (combined[i] == combined[j]) {
-                ++j;
-            }
-            
-            kmp_table[i] = j;
+        unsigned d = 16777619, h = 0, rh = 0, p = 1, maxLen = 0;
+        for (int i = 0; i < s.size(); ++i) {
+            h = h * d + s[i] - 'a';
+            rh += (s[i] - 'a') * p;
+            p *= d;
+            if (h == rh) maxLen = i + 1;
         }
-        
-        // Length of the longest palindromic prefix
-        int longest_palindrome_length = kmp_table.back();
-        
-        // Append the non-palindromic suffix from the reversed string to the front of the original string
-        return rev_s.substr(0, n - longest_palindrome_length) + s;
+        return string(rbegin(s), rbegin(s) + s.size() - maxLen) + s;
     }
 };
