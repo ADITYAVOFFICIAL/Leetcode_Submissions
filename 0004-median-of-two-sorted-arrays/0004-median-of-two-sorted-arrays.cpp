053e33990e4b5314085396da60b994d9a03928c4
ADITYA VERMA <adityav1304@gmail.com>
2024-09-26 05:39:54 +0530

    Time: 22 ms (70.96%), Space: 94.8 MB (70.76%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -1,14 +1,43 @@
-#include<algorithm>
 class Solution {
 public:
-    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        nums1.insert(nums1.end(), nums2.begin(), nums2.end());//inserting the nums2 to nums1
-        sort(nums1.begin(), nums1.end());//now sorting
-        int n = nums1.size();//this will calculate the size
-                if (n % 2 == 0) {//if divisible by 2 so we have two elements in the middle
-            return (nums1[n/2 - 1] + nums1[n/2]) / 2.0;
-        } else {//single median value
-            return nums1[n/2];
+    double findMedianSortedArrays(vector<int> &nums1, vector<int> &nums2) {
+        int n1 = nums1.size(), n2 = nums2.size();//getting size of the vector arrays
+        
+        if (n1 > n2)//checking if n2 is smaller than n1
+            return findMedianSortedArrays(nums2, nums1);//recursion
+        
+        int n = n1 + n2;//total length
+        int left = (n1 + n2 + 1) / 2; // elements on left of partition
+        int low = 0, high = n1;//lower bound and upper bound
+        
+        while (low <= high) {
+            int mid1 = (low + high) >> 1;
+            int mid2 = left - mid1; 
+            
+            int l1 = INT_MIN, l2 = INT_MIN, r1 = INT_MAX, r2 = INT_MAX;
+            
+            // Determine values of l1, l2, r1, and r2
+            if (mid1 < n1) r1 = nums1[mid1];
+            if (mid2 < n2) r2 = nums2[mid2];
+            if (mid1 - 1 >= 0) l1 = nums1[mid1 - 1];
+            if (mid2 - 1 >= 0) l2 = nums2[mid2 - 1];
+            
+            if (l1 <= r2 && l2 <= r1) {
+                if (n % 2 == 1)
+                    return max(l1, l2);
+                else
+                    return ((double)(max(l1, l2) + min(r1, r2))) / 2.0;
+            }
+            else if (l1 > r2) {
+                // Move towards the left side of nums1
+                high = mid1 - 1;
+            }
+            else {
+                // Move towards the right side of nums1
+                low = mid1 + 1;
+            }
         }
+        
+        return 0; 
     }
 };
